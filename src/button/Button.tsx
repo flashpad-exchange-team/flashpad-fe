093ddbcf35f7a5ae67adbbb2aa13e9f14fbaa5e5
Linh Nguyen <linhnguyendeveloper@gmail.com>
2023-08-06 00:33:11 +0700

    fix build

--- src/button/Button.tsx
+++ src/button/Button.tsx
@@ -1,21 +1,12 @@
-import className from 'classnames';
 import { MouseEventHandler, ReactNode } from 'react';
 
 type IButtonProps = {
   xl?: boolean;
   children: ReactNode;
-  onClick: MouseEventHandler<HTMLButtonElement>;
+  onClick?: MouseEventHandler<HTMLButtonElement>;
 };
 
 const Button = (props: IButtonProps) => {
-  const btnClass = className({
-    btn: true,
-    'btn-xl': props.xl,
-    'btn-base': !props.xl,
-    'btn-primary': true,
-    'bordered': true
-
-  });
 
   return (
     <button {...props} type="button" className="px-8 py-3 font-semibold rounded-full dark:bg-gray-100 dark:text-gray-800">
