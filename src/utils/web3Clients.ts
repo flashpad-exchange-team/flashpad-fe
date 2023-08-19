1c6f764d87b9ed44aa1ff7097243118eff33ff66
Hoang Khanh Nguyen <hoang.nguyen2@opskins.com>
2023-08-19 16:33:39 +0700

    Config infura RPC provider for linea testnet

--- src/utils/web3Clients.ts
+++ src/utils/web3Clients.ts
@@ -1,9 +1,10 @@
 import { createPublicClient, createWalletClient, custom, http } from 'viem'
 import { lineaTestnet } from 'viem/chains'
+import { LINEA_GOERLI_INFURA_RPC } from './constants';
 
 const publicClient: any = createPublicClient({
   chain: lineaTestnet,
-  transport: http()
+  transport: http(LINEA_GOERLI_INFURA_RPC)
 });
 
 const walletClient = createWalletClient({
