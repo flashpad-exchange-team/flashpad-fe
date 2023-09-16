e22d7ee937f63b7f06631bdc1e71756b96459adf
Hoang Khanh Nguyen <hoang.nguyen2@opskins.com>
2023-09-17 00:33:16 +0700

    Refactored routes and pages

--- src/pages/liquidity/index.tsx
+++ src/pages/liquidity/index.tsx
@@ -1,35 +1,18 @@
 // import { useLoading } from '@/context/LoadingContext';
 import LiquidityIcon from '@/icons/LiquidityIcon';
 import dynamic from 'next/dynamic';
-import { useState } from 'react';
-import PoolList from './components/PoolList';
 
 const TradeForm: any = dynamic(() => import('./components/TradeForm'), {
   ssr: false,
 }); // Disable SSR
 
 const Liquidity = () => {
-  const [isAddLiquidity, setIsAddLiquidity] = useState(true);
-  const handleClickViewExistingPosition = () => {
-    setIsAddLiquidity(false);
-  };
-
   return (
-    <>
-      <div className={isAddLiquidity ? '' : 'hidden'}>
-        <TradeForm
-          inputTitle1="Token 1"
-          inputTitle2="Token 2"
-          dividerIcon={<LiquidityIcon />}
-          setIsAddLiquidity={setIsAddLiquidity}
-          handleClickViewExistingPosition={handleClickViewExistingPosition}
-        />
-      </div>
-      <PoolList
-        setIsAddLiquidity={setIsAddLiquidity}
-        isAddLiquidity={isAddLiquidity}
-      />
-    </>
+    <TradeForm
+      inputTitle1="Token 1"
+      inputTitle2="Token 2"
+      dividerIcon={<LiquidityIcon />}
+    />
   );
 };
 
