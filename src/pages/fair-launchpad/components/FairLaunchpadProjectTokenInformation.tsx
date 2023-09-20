58d266dc7062d887ad461ac7df04165487b93471
ngoc <ngocle61>
2023-09-20 23:59:40 +0700

    handle form launchpad

--- src/pages/fair-launchpad/components/FairLaunchpadProjectTokenInformation.tsx
+++ src/pages/fair-launchpad/components/FairLaunchpadProjectTokenInformation.tsx
@@ -1,5 +1,79 @@
+import Pencil from '@/icons/Pencil';
 import React from 'react';
 
-export const FairLaunchpadProjectTokenInformation = () => {
-  return <div>FairLaunchpadProjectTokenInformation</div>;
+export const FairLaunchpadProjectTokenInformation = ({
+  values,
+  handleChange,
+}: any) => {
+  return (
+    <>
+      <div className="text-2xl text-bold text-center mt-6">
+        3. Token Infomation
+      </div>
+      <div className="flex flex-wrap">
+        <div className="w-full ">
+          <div className="mt-5 text-[#98A2B3]">Token Name</div>
+          <div className="relative">
+            <input
+              className="w-full bg-darkBlue h-[44px] pl-3 text-sm  mb-2 mt-2 rounded-md focus:outline-none placeholder-[#667085]"
+              placeholder="Typing"
+              name="tokenName"
+              value={values.tokenName}
+              onChange={handleChange}
+            />
+            <div className="absolute right-[20px] bottom-[50%] transform translate-y-[50%]">
+              <Pencil />
+            </div>
+          </div>
+        </div>
+        <div className="w-full">
+          <div className="mt-5 text-[#98A2B3]">Token Symbol</div>
+          <div className="relative">
+            <input
+              className="w-full bg-darkBlue h-[44px] pl-3 text-sm  mb-2 mt-2 rounded-md focus:outline-none placeholder-[#667085]"
+              placeholder="Typing"
+              name="tokenSymbol"
+              value={values.tokenSymbol}
+              onChange={handleChange}
+            />
+            <div className="absolute right-[20px] bottom-[50%] transform translate-y-[50%]">
+              <Pencil />
+            </div>
+          </div>
+        </div>
+        <div className="w-full">
+          <div className="mt-5 text-[#98A2B3]">Initial Supply</div>
+          <div className="relative">
+            <input
+              className="w-full bg-darkBlue h-[44px] pl-3 text-sm  mb-2 mt-2 rounded-md focus:outline-none placeholder-[#667085]"
+              placeholder="Typing"
+              name="tokenSupply"
+              value={values.tokenSupply}
+              onChange={handleChange}
+            />
+            <div className="absolute right-[20px] bottom-[50%] transform translate-y-[50%]">
+              <Pencil />
+            </div>
+          </div>
+        </div>
+        <div className="w-full">
+          <div className="mt-5 text-[#98A2B3]">
+            Token Type (ERC-20, BEP-20, etc.)
+          </div>
+          <div className="relative">
+            <input
+              className="w-full bg-darkBlue h-[44px] pl-3 text-sm  mb-2 mt-2 rounded-md focus:outline-none placeholder-[#667085]"
+              placeholder="Typing"
+              name="tokenType"
+              value={values.tokenType}
+              onChange={handleChange}
+            />
+            <div className="absolute right-[20px] bottom-[50%] transform translate-y-[50%]">
+              <Pencil />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
--- src/pages/fair-launchpad/index.tsx
+++ src/pages/fair-launchpad/index.tsx
@@ -1,146 +1,207 @@
 import { Button } from '@/components/button/Button';
 import ArrowRight from '@/icons/ArrowRight';
-import PenCil from '@/icons/Pencil';
-import Radio from '@/icons/Radio';
-import { useState } from 'react';
+import BackIcon from '@/icons/BackIcon';
+import SwapLeftIcon from '@/icons/SwapLeft';
+import SwapRightIcon from '@/icons/SwapRight';
+import { useMemo, useState } from 'react';
+import { FairLaunchpadProjectDetail } from './components/FairLaunchpadProjectDetail';
+import { FairLaunchpadProjectDocuments } from './components/FairLaunchpadProjectDocuments';
+import { FairLaunchpadProjectProposition } from './components/FairLaunchpadProjectProposition';
+import { FairLaunchpadProjectSocialMedia } from './components/FairLaunchpadProjectSocialMedia';
+import { FairLaunchpadProjectTeamInformation } from './components/FairLaunchpadProjectTeamInformation';
+import { FairLaunchpadProjectTokenInformation } from './components/FairLaunchpadProjectTokenInformation';
+import { FairLaunchpadRoadmapAndDistribution } from './components/FairLaunchpadRoadmapAndDistribution';
 
 const Launchpad = () => {
-  const [index, setIndex] = useState(1);
-  const handleChooseOption = (option: number) => {
-    setIndex(option);
+  const [step, setStep] = useState(1);
+  const [formValues, setFormValues] = useState({
+    projectName: '',
+    projectWebsite: '',
+    projectDescription: '',
+    projectCategory: '',
+    teamName: '',
+    teamEmail: '',
+    teamExp: '',
+    tokenName: '',
+    tokenSymbol: '',
+    tokenSupply: '',
+    tokenType: '',
+    brieflyProblemProjectSolves: '',
+    valueYourProject: '',
+    roadMapLink: '',
+    roadMapInfo: '',
+    tokenomicLink: '',
+    socialTelegramChannel: '',
+    socialTelegramGroup: '',
+    socialTwitter: '',
+    socialDiscord: '',
+    documentsWhitepaper: '',
+    documentsPitchDeck: '',
+    documentsTechnical: '',
+    documentsLegal: '',
+  });
+  const handleChange = (e: any) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
   };
 
+  let componentRender;
+  switch (step) {
+    case 1:
+      componentRender = (
+        <FairLaunchpadProjectDetail
+          values={formValues}
+          handleChange={handleChange}
+        />
+      );
+      break;
+    case 2:
+      componentRender = (
+        <FairLaunchpadProjectTeamInformation
+          values={formValues}
+          handleChange={handleChange}
+        />
+      );
+      break;
+    case 3:
+      componentRender = (
+        <FairLaunchpadProjectTokenInformation
+          values={formValues}
+          handleChange={handleChange}
+        />
+      );
+      break;
+    case 4:
+      componentRender = (
+        <FairLaunchpadProjectProposition
+          values={formValues}
+          handleChange={handleChange}
+        />
+      );
+      break;
+    case 5:
+      componentRender = (
+        <FairLaunchpadRoadmapAndDistribution
+          values={formValues}
+          handleChange={handleChange}
+        />
+      );
+      break;
+    case 6:
+      componentRender = (
+        <FairLaunchpadProjectSocialMedia
+          values={formValues}
+          handleChange={handleChange}
+        />
+      );
+      break;
+    case 7:
+      componentRender = (
+        <FairLaunchpadProjectDocuments
+          values={formValues}
+          handleChange={handleChange}
+        />
+      );
+      break;
+    default:
+      <FairLaunchpadProjectDetail
+        values={formValues}
+        handleChange={handleChange}
+      />;
+  }
+  const submitForm = () => {
+    console.log('Submit Form');
+  };
+
+  const handleNextForm = () => {
+    if (step === 7) {
+      submitForm();
+    } else {
+      setStep(step + 1);
+    }
+  };
+
+  const handleBackForm = () => {
+    setStep(step - 1);
+  };
+
+  const isNextDisabled = useMemo(() => {
+    let isDisabled = true;
+    if (step === 1) {
+      isDisabled =
+        !formValues.projectName ||
+        !formValues.projectName ||
+        !formValues.projectDescription ||
+        !formValues.projectCategory;
+    }
+    if (step === 2) {
+      isDisabled =
+        !formValues.teamName || !formValues.teamEmail || !formValues.teamExp;
+    }
+    if (step === 3) {
+      isDisabled =
+        !formValues.tokenName ||
+        !formValues.tokenSymbol ||
+        !formValues.tokenSupply ||
+        !formValues.tokenType;
+    }
+    if (step === 4) {
+      isDisabled =
+        !formValues.brieflyProblemProjectSolves || !formValues.valueYourProject;
+    }
+    if (step === 5) {
+      isDisabled =
+        !formValues.roadMapLink ||
+        !formValues.roadMapInfo ||
+        !formValues.tokenomicLink;
+    }
+    if (step === 6) {
+      isDisabled =
+        !formValues.socialTelegramChannel ||
+        !formValues.socialTelegramGroup ||
+        !formValues.socialTwitter ||
+        !formValues.socialDiscord;
+    }
+    if (step === 7) {
+      isDisabled =
+        !formValues.documentsWhitepaper ||
+        !formValues.documentsPitchDeck ||
+        !formValues.documentsTechnical ||
+        !formValues.documentsLegal;
+    }
+
+    return isDisabled;
+  }, [step, formValues]);
+
+  console.log(isNextDisabled, 'isNextDisabled');
   return (
     <>
-      <div className="max-w-[1096px] p-8 md:p-0 w-full mx-auto">
-        <div className="my-10 bg-dark px-4">
-          <div className="w-full text-lg py-3 text-left">
+      <div className="max-w-[768px] p-8 md:p-0 w-full mx-auto">
+        <div className="my-10 bg-dark px-4 pt-8 pb-4">
+          <div className="text-2xl text-bold mx-auto w-full flex items-center gap-3 justify-center">
+            <SwapLeftIcon />
             Support Request Form
+            <SwapRightIcon />
           </div>
-          <div className="flex flex-col md:flex-row gap-3">
-            <div
-              onClick={() => handleChooseOption(1)}
-              className="h-[80px] w-full md:w-1/3 rounded-lg border border-[#150E39] flex items-center justify-center"
+          {componentRender}
+          <div className="flex justify-end">
+            {step !== 1 && (
+              <Button
+                type="secondary"
+                className="w-full md:w-1/4 justify-center my-4 h-[52px] text-base px-[42px] mr-5"
+                onClick={handleBackForm}
+              >
+                <BackIcon /> Back
+              </Button>
+            )}
+            <Button
+              className="w-full md:w-1/4 justify-center my-4 h-[52px] text-base px-[42px]"
+              onClick={handleNextForm}
+              disabled={isNextDisabled}
             >
-              <Radio active={index === 1} />
-              <span className={`ml-4 ${index === 1 ? '' : 'text-[#474747]'} `}>
-                Ask For Funds
-                <span className="opacity-0 md:hidden">coins</span>
-              </span>
-            </div>
-            <div
-              onClick={() => handleChooseOption(2)}
-              className="h-[80px] w-full md:w-1/3 rounded-lg border border-[#150E39] flex items-center justify-center"
-            >
-              <Radio active={index === 2} />
-              <span className={`ml-4 ${index === 2 ? '' : 'text-[#474747]'} `}>
-                Ask for Incubator
-                <span className="opacity-0 md:hidden">co</span>
-              </span>
-            </div>
-            <div
-              onClick={() => handleChooseOption(3)}
-              className="h-[80px] w-full md:w-1/3 rounded-lg border border-[#150E39] flex items-center justify-center"
-            >
-              <Radio active={index === 3} />
-              <span className={`ml-4 ${index === 3 ? '' : 'text-[#474747]'} `}>
-                Ask for Partnership
-              </span>
-            </div>
+              {step !== 7 ? 'Next' : 'Send'} <ArrowRight fill />
+            </Button>
           </div>
-          <div className="flex flex-wrap">
-            <div className="w-full md:w-1/2">
-              <div className="mt-5 text-[#98A2B3]">Name/Company</div>
-              <div className="relative">
-                <input
-                  className="w-full bg-darkBlue h-[44px] pl-3 text-sm  mb-2 mt-2 rounded-md focus:outline-none placeholder-[#667085]"
-                  placeholder="Typing"
-                />
-                <div className="absolute right-[20px] bottom-[50%] transform translate-y-[50%]">
-                  <PenCil />
-                </div>
-              </div>
-            </div>
-            <div className="w-full md:w-1/2 md:pl-3">
-              <div className="mt-5 text-[#98A2B3]">Name/Company</div>
-              <div className="relative">
-                <input
-                  className="w-full bg-darkBlue h-[44px] pl-3 text-sm  mb-2 mt-2 rounded-md focus:outline-none placeholder-[#667085]"
-                  placeholder="Typing"
-                />
-                <div className="absolute right-[20px] bottom-[50%] transform translate-y-[50%]">
-                  <PenCil />
-                </div>
-              </div>
-            </div>
-            <div className="w-full md:w-1/2">
-              <div className="mt-5 text-[#98A2B3]">Website</div>
-              <div className="relative">
-                <input
-                  className="w-full bg-darkBlue h-[44px] pl-3 text-sm  mb-2 mt-2 rounded-md focus:outline-none placeholder-[#667085]"
-                  placeholder="Typing"
-                />
-                <div className="absolute right-[20px] bottom-[50%] transform translate-y-[50%]">
-                  <PenCil />
-                </div>
-              </div>
-            </div>
-            <div className="w-full md:w-1/2 md:pl-3">
-              <div className="mt-5 text-[#98A2B3]">Field of Activity</div>
-              <div className="relative">
-                <input
-                  className="w-full bg-darkBlue h-[44px] pl-3 text-sm  mb-2 mt-2 rounded-md focus:outline-none placeholder-[#667085]"
-                  placeholder="Typing"
-                />
-                <div className="absolute right-[20px] bottom-[50%] transform translate-y-[50%]">
-                  <PenCil />
-                </div>
-              </div>
-            </div>
-            <div className="w-full md:w-1/2">
-              <div className="mt-5 text-[#98A2B3]">Telegram</div>
-              <div className="relative">
-                <input
-                  className="w-full bg-darkBlue h-[44px] pl-3 text-sm  mb-2 mt-2 rounded-md focus:outline-none placeholder-[#667085]"
-                  placeholder="Typing"
-                />
-                <div className="absolute right-[20px] bottom-[50%] transform translate-y-[50%]">
-                  <PenCil />
-                </div>
-              </div>
-            </div>
-            <div className="w-full md:w-1/2 md:pl-3">
-              <div className="mt-5 text-[#98A2B3]">Email</div>
-              <div className="relative">
-                <input
-                  className="w-full bg-darkBlue h-[44px] pl-3 text-sm  mb-2 mt-2 rounded-md focus:outline-none placeholder-[#667085]"
-                  placeholder="Typing"
-                />
-                <div className="absolute right-[20px] bottom-[50%] transform translate-y-[50%]">
-                  <PenCil />
-                </div>
-              </div>
-            </div>
-            <div className="w-full md:w-1/2">
-              <div className="mt-5 text-[#98A2B3]">Link Documents</div>
-              <div className="relative">
-                <input
-                  className="w-full bg-darkBlue h-[44px] pl-3 text-sm  mb-2 mt-2 rounded-md focus:outline-none placeholder-[#667085]"
-                  placeholder="Typing"
-                />
-                <div className="absolute right-[20px] bottom-[50%] transform translate-y-[50%]">
-                  <PenCil />
-                </div>
-              </div>
-            </div>
-            <div className="w-full">
-              <div className="mt-5 text-[#98A2B3]">Request Description</div>
-              <textarea className="w-full bg-darkBlue h-[160px] pl-3 text-sm  mb-2 mt-2 rounded-md focus:outline-none placeholder-[#667085]" />
-            </div>
-          </div>
-          <Button className="w-full md:w-1/6 justify-center my-4 h-[52px] text-base px-[42px]">
-            Send <ArrowRight fill />
-          </Button>
         </div>
       </div>
     </>
