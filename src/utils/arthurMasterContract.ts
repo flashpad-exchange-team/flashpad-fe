3361761b8fb0648742622f32b821c222e7a3f9d4
Hoang Khanh Nguyen <hoang.nguyen2@opskins.com>
2023-10-02 21:02:45 +0700

    Added util module Arthur Master contract

---
+++ src/utils/arthurMasterContract.ts
@@ -0,0 +1,67 @@
+import { Address } from 'viem';
+import { abi as ArthurMasterABI } from '@/resources/abis/ArthurMaster.json';
+import { publicClient, walletClient } from './web3Clients';
+import { handleError } from './handleError';
+
+export const read = async (
+  address: Address,
+  functionName: string,
+  args: any[]
+) => {
+  try {
+    const result = await publicClient.readContract({
+      address,
+      abi: ArthurMasterABI,
+      functionName,
+      args,
+    });
+    return mapResultArrayToObj(functionName, result);
+  } catch (err: any) {
+    console.log(err);
+    return undefined;
+  }
+};
+
+export const write = async (
+  account: Address,
+  address: Address,
+  functionName: string,
+  args: any[]
+) => {
+  try {
+    const { request, result } = await publicClient.simulateContract({
+      account,
+      address,
+      abi: ArthurMasterABI,
+      functionName,
+      args,
+    });
+    const hash = await walletClient.writeContract(request);
+    return { hash, result };
+  } catch (err: any) {
+    handleError(err);
+    return undefined;
+  }
+};
+
+const getPoolInfoResultKeys = [
+  'poolAddress',
+  'allocPoint',
+  'lastRewardTime',
+  'reserve',
+  'poolEmissionRate'
+];
+
+const functionResultKeysMap: { [k: string]: string[] } = {
+  'getPoolInfo': getPoolInfoResultKeys,
+};
+
+const mapResultArrayToObj = (functionName: string, result: any) => {
+  const props = functionResultKeysMap[functionName];
+  if (!props) return result;
+  const resObj: { [k: string]: any } = {};
+  props.forEach((prop, i) => {
+    resObj[prop] = result[i];
+  });
+  return resObj;
+}
