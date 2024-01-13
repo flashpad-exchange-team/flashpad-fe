103cbf0680067e5bc35e6e27e948d7b6ac27d655
Linh Nguyen <linhnguyendeveloper@gmail.com>
2024-01-14 00:45:15 +0700

    add token list and import token ui

--- src/components/modal/SelectTokenModal.tsx
+++ src/components/modal/SelectTokenModal.tsx
@@ -12,6 +12,7 @@ import * as erc20TokenContract from '@/utils/contract/erc20TokenContract';
 import { getBalance } from '@/utils/web3Helpers';
 import Image from 'next/image';
 import BigNumber from 'bignumber.js';
+import InlineLoading from '../loading/InlineLoading';
 
 export interface SelectTokenModalProps {
   toggleOpen: () => void;
@@ -26,6 +27,9 @@ const SelectTokenModal = ({
 }: SelectTokenModalProps) => {
   const { address: userAddress } = useAccount();
   const [search, setSearch] = useState<string>('');
+  const [newToken, setNewToken] = useState<any>({});
+  const [loadingSearch, setLoadingSearch] = useState<boolean>(false);
+
   const [tokensList, setTokensList] = useState<IERC20TokenMetadata[]>(
     CHAINS_TOKENS_LIST.map((e) => {
       return {
@@ -56,12 +60,12 @@ const SelectTokenModal = ({
                     [userAddress]
                   ),
             ]);
-
             resolve({
               ...token,
-              curBalance: BigNumber(balance || '0')
-                .div(BigNumber(10).pow(decimals))
-                .toFixed(2),
+              curBalance:
+                BigNumber(balance || '0')
+                  .div(BigNumber(10).pow(decimals))
+                  .toFixed(2) || '0.00',
             });
           } catch (error) {
             console.error(error);
@@ -77,27 +81,67 @@ const SelectTokenModal = ({
     setTokensList(tokensListWithBalances);
     setTokensListFiltered(tokensListWithBalances);
   };
-
   useEffect(() => {
     fetchTokenBalances();
   }, [userAddress, isOpen]);
-
-  const onSearchChange = (e: any) => {
+  console.log(tokensList);
+  const onSearchChange = async (e: any) => {
     const text = e.target.value;
     setSearch(text);
+
     if (text) {
       const lowercased = text.toLowerCase();
-      setTokensListFiltered(
-        tokensList.filter(
-          (item: any) => item.symbol.toLowerCase().includes(lowercased)
-            || item.name.toLowerCase().includes(lowercased)
-        )
+      const listAfterFilter = tokensList.filter(
+        (item: any) =>
+          item.symbol.toLowerCase().includes(lowercased) ||
+          item.name.toLowerCase().includes(lowercased) ||
+          item.address.includes(text)
       );
+      setTokensListFiltered(listAfterFilter);
+      if (listAfterFilter.length === 0) {
+        if (
+          text.includes('0x') &&
+          text.length === 42 &&
+          !tokensList.some((item) => item.address === text)
+        ) {
+          setLoadingSearch(true);
+          const tokenDecimals = (await erc20TokenContract.erc20Read(
+            text,
+            'decimals',
+            []
+          )) as bigint;
+          const tokenSymbol = (await erc20TokenContract.erc20Read(
+            text,
+            'symbol',
+            []
+          )) as string;
+          const tokenName = (await erc20TokenContract.erc20Read(
+            text,
+            'name',
+            []
+          )) as string;
+          setNewToken({
+            decimals: tokenDecimals,
+            name: tokenName,
+            symbol: tokenSymbol,
+            address: text,
+          });
+          setTokensList([
+            ...tokensList,
+            {
+              decimals: +tokenDecimals.toString(),
+              name: tokenName,
+              symbol: tokenSymbol,
+              address: text,
+            },
+          ]);
+        } else setNewToken({});
+        setLoadingSearch(false);
+      } else setNewToken({});
     } else {
       setTokensListFiltered(tokensList);
     }
   };
-
   return (
     <CommonModal isOpen={isOpen} onRequestClose={toggleOpen} width="550px">
       <div className="flex items-center justify-between w-full mt-4 lg:mt-0">
@@ -118,62 +162,127 @@ const SelectTokenModal = ({
         onChange={onSearchChange}
       />
       <div className="max-h-[450px] overflow-y-auto pr-3">
-        <div className="text-lg font-semibold my-2">Common bases</div>
-        <div className="flex flex-wrap ">
-          {tokensList.slice(0, 8).map((item: any, index: number) => (
-            <div
-              className={`w-2/4 lg:w-1/4`}
-              key={item.symbol}
-              onClick={() => {
-                if (selectValue) selectValue(item);
-                toggleOpen();
-              }}
-            >
-              <div
-                className={`flex gap-1 items-center hover:bg-[#1D2939] rounded-md px-1 py-2 w-[90px]  ${
-                  index % 2 == 1 ? 'mr-0 ml-auto' : ''
-                } lg:mr-0 lg:ml-0`}
-                onClick={toggleOpen}
-              >
-                {item.logoURI ? (
-                  <Image alt="logo" src={item.logoURI} width={25} height={25} />
-                ) : (
-                  <BNBICon />
-                )}
-                {item.symbol}
-              </div>
+        {search ? null : (
+          <>
+            {' '}
+            <div className="text-lg font-semibold my-2">Common bases</div>
+            <div className="flex flex-wrap ">
+              {tokensList.slice(0, 8).map((item: any, index: number) => (
+                <div
+                  className={`w-2/4 lg:w-1/4`}
+                  key={item.symbol}
+                  onClick={() => {
+                    if (selectValue) {
+                      setSearch('');
+                      selectValue(item);
+                      setNewToken({});
+                    }
+                    toggleOpen();
+                  }}
+                >
+                  <div
+                    className={`flex gap-1 items-center hover:bg-[#1D2939]  cursor-pointer rounded-md px-1 py-2 w-[90px]  ${
+                      index % 2 == 1 ? 'mr-0 ml-auto' : ''
+                    } lg:mr-0 lg:ml-0`}
+                    onClick={toggleOpen}
+                  >
+                    {item.logoURI ? (
+                      <Image
+                        alt="logo"
+                        src={item.logoURI}
+                        width={25}
+                        height={25}
+                      />
+                    ) : (
+                      <BNBICon />
+                    )}
+                    {item.symbol}
+                  </div>
+                </div>
+              ))}
             </div>
-          ))}
-        </div>
-        <div className="text-lg font-semibold my-2">Tokens list</div>
-        {tokensListFiltered
-          .sort((a, b) => b.curBalance - a.curBalance)
-          .map((item: any) => (
-            <div
-              className="flex justify-between items-center my-2 hover:bg-[#1D2939] rounded-md px-1 py-2"
-              key={item.symbol}
-              onClick={() => {
-                if (selectValue) selectValue(item);
-                toggleOpen();
-              }}
-            >
-              <div className="flex items-center gap-2">
-                {item.logoURI ? (
-                  <Image alt="logo" src={item.logoURI} width={25} height={25} />
-                ) : (
-                  <BNBICon />
-                )}
+          </>
+        )}
 
-                <div>
-                  <div className="text-sm">{item.symbol}</div>
-                  <div className="text-xs text-[#475467]">{item.name}</div>
+        <div className="text-lg font-semibold my-2">Tokens list</div>
+        {loadingSearch ? (
+          <div className="ml-2 mb-3">
+            <InlineLoading />
+          </div>
+        ) : (
+          <>
+            {' '}
+            {newToken?.name && (
+              <div
+                className="flex justify-between items-center my-2 hover:bg-[#1D2939] rounded-md px-1 py-2 cursor-pointer"
+                key={newToken.symbol}
+                onClick={() => {
+                  if (selectValue) {
+                    selectValue(newToken);
+                    setNewToken({});
+                    setSearch('');
+                  }
+                  fetchTokenBalances();
+                  toggleOpen();
+                }}
+              >
+                <div className="flex items-center gap-2">
+                  <BNBICon />
+                  <div>
+                    <div className="text-sm">{newToken.symbol}</div>
+                    <div className="text-xs text-[#475467]">
+                      {newToken.name}
+                    </div>
+                  </div>
+                  <div className="text-white text-sm rounded-md px-2 ml-1 py-1 bg-[#323a4a]">
+                    Import
+                  </div>
+                </div>
+                <div className="text-sm md:text-lg pr-2 break-all pl-16 md:pl-0">
+                  {/* {item.curBalance !== 'NaN' ? item.curBalance : '0.00'} */}
+                  0.00
                 </div>
               </div>
-              <div className="text-sm md:text-lg pr-2 break-all pl-16 md:pl-0">
-                {item.curBalance}
-              </div>
-            </div>
-          ))}
+            )}
+            {tokensListFiltered
+              .sort((a, b) => b.curBalance - a.curBalance)
+              .map((item: any) => (
+                <div
+                  className="flex justify-between items-center my-2 hover:bg-[#1D2939] rounded-md px-1 py-2  cursor-pointer"
+                  key={item.symbol}
+                  onClick={() => {
+                    if (selectValue) {
+                      setSearch('');
+                      setNewToken({});
+                      selectValue(item);
+                    }
+                    toggleOpen();
+                  }}
+                >
+                  <div className="flex items-center gap-2">
+                    {item.logoURI ? (
+                      <Image
+                        alt="logo"
+                        src={item.logoURI}
+                        width={25}
+                        height={25}
+                      />
+                    ) : (
+                      <BNBICon />
+                    )}
+
+                    <div>
+                      <div className="text-sm">{item.symbol}</div>
+                      <div className="text-xs text-[#475467]">{item.name}</div>
+                    </div>
+                  </div>
+                  <div className="text-sm md:text-lg pr-2 break-all pl-16 md:pl-0">
+                    {item.curBalance !== 'NaN' ? item.curBalance : '0.00'}
+                  </div>
+                </div>
+              ))}
+          </>
+        )}
       </div>
       <Button
         onClick={toggleOpen}
