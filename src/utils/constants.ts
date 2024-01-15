c8e743d4ce8f1498b928f707f5fc191eb2195ed2
Hoang Khanh Nguyen <hoang.nguyen2@opskins.com>
2024-01-16 02:08:08 +0700

    removed console.log

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -42,7 +42,7 @@ export const APP_BASE_CHAIN = IS_LINEA
     ? linea
     : lineaTestnet
   : polygonMumbai;
-console.log({ APP_BASE_CHAIN, IS_LINEA, ENVIRONMENT })
+
 // Linea network
 export const FLASHPAD_ROUTER_ADDRESS_LINEA =
   process.env.NEXT_PUBLIC_FLASHPAD_ROUTER_ADDRESS_LINEA ||
